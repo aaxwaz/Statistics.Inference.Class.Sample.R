f0e45126812f53ce67ed2e44af43c6d66c1936fc
aaxwaz <wangweimin777@gmail.com>
2014-12-24 08:56:56 +0800

    Update Permutation_test.R

--- Permutation_test.R
+++ Permutation_test.R
@@ -2,7 +2,7 @@ data(InsectSprays)
 subdata <- InsectSprays[InsectSprays$spray %in% c("B", "C"), ]
 y <- subdata$count
 group <- as.character(subdata$group)
-testStat <- function(w, g) mean(w[g=="B"]) - mean(w[g=="c"])
+testStat <- function(w, g) mean(w[g=="B"]) - mean(w[g=="C"])
 observedStat <- testStat(y, group)
 permutations <- sapply(1:10000, function(i) testStat(y, sample(group)))
 observedStat
